145843b2a5a0fda5106eb11a74be540fc7264c1f
jeffstienstra <jeffstienstra@gmail.com>
2021-10-08 08:08:36 -0400

    add reverse-string challenge

---
+++ reverse-string.rb
@@ -0,0 +1,12 @@
+# Write a function that returns the reverse of a given string.
+
+# Input: “abcde”
+# Output: “edcba”
+
+string = "abcde"
+
+def string_reverser(string)
+  return string.reverse
+end
+
+p string_reverser(string)
